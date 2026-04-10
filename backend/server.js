2e3205923604a9fd826c317933b9341d6116d2a3
Sameer <sameersaini8851@gmail.com>
2026-04-10 09:13:46 +0530

    he

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const app = express();
 
 // Security & Body parsing middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
